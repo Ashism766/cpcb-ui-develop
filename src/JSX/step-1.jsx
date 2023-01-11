bb7d43d33c86a3fec73253473f98cb42e8120c59
AshisMaity <maity.ashis651@gmail.com>
2023-01-11 17:28:53 +0530

    more improvement

--- src/JSX/step-1.jsx
+++ src/JSX/step-1.jsx
@@ -8,27 +8,23 @@ import "../style/step-1.css";
 
 const Middle = (props) =>{
 
-    const [cl, isClk] = React.useState(false);
-    const Cli = () =>{
-        isClk(!cl);
-        console.log(cl)
-    }
-
- 
-    return(<div style={cl?{"backgroundColor":"#323B4B"}:{"backgroundClip":"#fff"}} onClick={Cli}  className="squar">
+    return(<div style={{'backgroundColor':props.bc}}   className="squar">
             
-            <div style={cl?{"backgroundColor":"#3EFEA8"}:{"backgroundClip":"#fff"}} className="small-circle"></div>
+            <div style={{'backgroundColor':props.mc}} className="small-circle"></div>
             <div className="icon">
                 <img  src={require("../image/"+props.img)} alt={"It's a pic of key"}/>
             </div>
-            <div style={cl?{"color":"white"}:{"color":"black"}} className="text">{props.text||"Ground Station Data"}</div>
-            
-    </div>)
+            <div style={{"color":props.tc}} className="text">{props.text||"Ground Station Data"}</div>  
+    </div>);
+
 }
 
 
 const Step_1 = ()=>{
     
+    const [c1, setC1] = React.useState(false);
+    const [c2, setC2] = React.useState(false);
+    const [c3, setC3] = React.useState(false);
 
 
     return <div>
@@ -36,19 +32,19 @@ const Step_1 = ()=>{
         <Header/>
         <Glayer step = "Step 1" dis = "none" prevPage ="" 
             nextPage="step-2" text ="Select a Data Source"/>
+
+
         <div className="body-contain">
 
-            
-            
             <div className="main-body">
-            <span ><Middle className="Card1" id="1" text="Device Data" img = "Squ.png"/></span>
-            <span><Middle className="Card" id="2" text="Satellite Data" img="st.png"/></span>
-            <span><Middle className="Card" id="3" text="Ground Station Data" img="gst.png"/></span>
+            <div onClick={()=>{if(!c1){setC2(false); setC3(false)}setC1(!c1)}} className="dis" ><Middle bc={c1?"#323B4B":''} mc ={c1?"#3EFEA8":''} tc={c1?"white":''} text="Device Data" img = "Squ.png"/></div>
+            <div onClick={()=>{if(!c2){setC1(false); setC3(false)}setC2(!c2)}} className="dis" ><Middle bc={c2?"#323B4B":''} mc ={c2?"#3EFEA8":''} tc={c2?"white":''} text="Satellite Data" img="st.png"/></div>
+            <div onClick={()=>{if(!c3){setC2(false); setC1(false)}setC3(!c3)}} className="dis" ><Middle bc={c3?"#323B4B":''} mc ={c3?"#3EFEA8":''} tc={c3?"white":''} text="Ground Station Data" img="gst.png"/></div>
             </div>
 
         </div>
            
-         
+          
     </div>
 }
 
